3765696fe91e08779ccb156ec316f1376b63f1a8
ИСП-22 <-22@LAB30-05>
2024-10-10 13:26:11 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введи a = ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введи b = ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введи c = ");
+double c = Convert.ToDouble(Console.ReadLine());
+
+//вычисляем дискриминант
+double discriminant = b * b - 4 * a * c;
+
+//оперделяем тип корней
+if (discriminant > 0)
+{
+    Console.WriteLine("Корни действительные и различные");
+}
+else if (discriminant == 0)
+{
+    Console.WriteLine("Корни действительные и равные");
+}
+else
+{
+    Console.WriteLine("Корней нет");
+}
