d1cdd858dfe5d009074c5a7e538431ea9cb5d1ac
seaferz <86144706+seaferz@users.noreply.github.com>
2024-10-14 13:51:22 +0300

    Update Program.cs

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.Write("Введи a = ");
+/*В зависимости от параметров a, b, c квадратного уравнения вывести одно из 
+сообщений: «корней нет», «корни действительные и различные», «корни действительные 
+и равные».*/
+
+//Просим пользователя ввести параметры
+Console.Write("Введи a = ");
 double a = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введи b = ");
 double b = Convert.ToDouble(Console.ReadLine());
